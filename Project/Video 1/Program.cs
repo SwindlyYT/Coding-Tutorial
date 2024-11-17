e6b90bfafa5240ee603c88b6cc543ea4210e017e
Quinn <99457294+SwindlyYT@users.noreply.github.com>
2024-11-17 16:32:17 -0500

    Video 1 uploaded to YouTube

---
+++ Project/Video 1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+// Same namespaces are connected
+namespace Project
+{
+    // Class 
+    internal class Program
+    {
+        // Always runs
+        static void Main(string[] args)
+        {
+            // Declaring a string variable called str with no value
+            string str;
+
+            // Write lines
+            Console.WriteLine("Hello, World!");
+
+            // Read input from the console (\n which is new line which is enter key)
+            Console.ReadLine();
+
+            // Reads line from console and puts it into a string variable. 
+            str = Console.ReadLine(); // This warning doesnt mean much we arent doing proper error handling which will done later on
+
+            // Will write the line typed from the console and write it back out
+            Console.WriteLine(str + "\n");
+
+            // Read the input from the console and then it will print to the console. Inner most functions get called FIRST
+            Console.WriteLine(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Project/Video 1/Program_With_Documentation.cs
@@ -0,0 +1,35 @@
+﻿
+// Same as Program.cs except namespace is different (cant have same name classes and methods in the same namespace) and this one is more
+// heavily commented
+
+
+// Namespace of this script. Can be anything. Same namespaces are connected to one another
+namespace VideoOne
+{
+    // Class. Thats about it for now
+    internal class Program
+    {
+        // Main method. This will run at the start of the program
+        static void Main(string[] args)
+        {
+            // Declares a string variable called str that is not assigned a value
+            string str;
+
+            // This line prints to the console
+            Console.WriteLine("Hello, World!");
+
+            // Reads input from the console. Captures everything until the user presses enter
+            Console.ReadLine(); // (\n is what it appears as in code)
+
+            // Reads input from console and puts it into a string variable to be used later
+            str = Console.ReadLine(); // This warning is caused due to no error handling. This can be ignored.
+                                      // We will go over error handling in a future video
+
+            // Prints the string str to the console
+            Console.WriteLine(str);
+
+            // Reads input from the console then prints. Inner most functions get fully run FIRST
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
